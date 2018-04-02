cc4a757bf174566abaa87daf3ac50375be8630f2
KarLLooN <38017059+KarLLooN@users.noreply.github.com>
2018-04-02 23:13:15 +0300

    Add files via upload

---
+++ AbonentRepository.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.List;
+
+public interface AbonentRepository {
+
+    static Abonent findById() {
+        return null;
+    }
+
+    static void deliteById() {
+    }
+
+    static Abonent create() {
+        return null;
+    }
+
+    static Abonent update() {
+        return null;
+    }
+}
